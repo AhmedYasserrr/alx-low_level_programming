a68c7ab986947bead4e4079119212f031c0ee055
AhmedYasserrr <ahmdyasrj@gmail.com>
2023-08-13 18:21:47 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-print_alphabet.c
+ * Author: Ahmed Yasser
+ */
+#include <stdio.h>
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i= 97; i < 97+26; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
