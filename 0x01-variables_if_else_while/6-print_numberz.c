0a38c1a14c237f7d65cd503e71469af8f1b67572
AhmedYasserrr <ahmdyasrj@gmail.com>
2023-08-13 18:42:57 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+/*
+ * File: 5-print_numbers.c
+ * Author: Ahmed Yasser
+ */
+#include <stdio.h>
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 48 + 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
