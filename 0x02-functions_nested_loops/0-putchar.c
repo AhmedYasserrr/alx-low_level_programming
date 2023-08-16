b7321a09002fd09dae012dcb25bec962d99f0205
AhmedYasserrr <ahmdyasrj@gmail.com>
2023-08-15 20:11:28 -0700

    c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,11 @@
-/**
- * file name: 0-putchar.c
- * Author: Ahmed Yasser
- */
 #include "main.h"
 
 /**
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	/*prints _putchar*/
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -22,6 +16,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	/* Return: Always 0 (Success)*/
 	return (0);
 }
