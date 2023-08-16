c175b969d2eed0006ef8213d99ac1e5f5d115e80
AhmedYasserrr <ahmdyasrj@gmail.com>
2023-08-15 21:45:44 -0700

    c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,12 @@
-/*
- * File: 2-print_alphabet.c
- * Author: Ahmed Yasser
- */
 #include "main.h"
+
 /**
  * main - Prints a random number and states whether
  *        it is positive, negative, or zero.
  *
  * Return: Always 0
  */
+
 void print_alphabet_x10(void)
 {
 	int i;
