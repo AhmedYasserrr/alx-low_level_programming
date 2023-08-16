87feea2d0f61fcb25febeedf4f1d08ca20905cf0
AhmedYasserrr <ahmdyasrj@gmail.com>
2023-08-15 20:24:55 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+/*
+ * File: 2-print_alphabet.c
+ * Author: Ahmed Yasser
+ */
+#include "main.h"
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 97 + 26; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
