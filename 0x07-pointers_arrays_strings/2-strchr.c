ba4b4907e87a509cab463fd7c7eeb31159ade45d
AhmedYasserrr <ahmdyasrj@gmail.com>
2023-08-28 16:56:16 -0700

    c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - fill a block of memory with a specific value
+ * @s: starting address of memory passed to function
+ * @c: the filling value
+ *
+ * Return: pointer to the first occurrence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	while (s != '\0')
+	{
+		if (*s == c)
+			break;
+		s++;
+	}
+	return (s);
+}
