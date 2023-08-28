f17401a228b334efcc8dd195468c24288a5608a3
AhmedYasserrr <ahmdyasrj@gmail.com>
2023-08-28 16:33:43 -0700

    c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,9 +5,8 @@
  * @b: the filling value
  * @n: number of units to be changed
  *
- * Return: changed array with new value 
+ * Return: changed array with new value
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
